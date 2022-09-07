009d359fcd75484558a6f3bc55345317d700a75f
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-09-07 18:15:59 +0200

    frog movin

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ const startPauseButton = document.querySelector('#start-pause-button')
 
 const squares = document.querySelectorAll('.grid div')
 
-function moveFrog() {
-    console.log('moved')
+let currentIndex = 76
+function moveFrog(e) {
+
+    switch(e.key) {
+       case 'ArrowLeft' :
+            console.log('move left')
+            break
+       case 'ArrowRight' :
+            console.log('move right')
+            break
+        case 'ArrowUp' :
+            console.log('move up')
+            break
+        case 'ArrowDown' :
+            console.log('move Down')
+             break
+    }
+    
+    squares[currentIndex].classList.add('frog')
 }
 
 document.addEventListener('keyup', moveFrog)
