9bcc9710469081b5d8384d9c31bd5587263e2fdc
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-09-07 18:22:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const timeLeftDisplay = document.querySelector('#time-left')
 const  resultDisplay = document.querySelector('#result')
 const startPauseButton = document.querySelector('#start-pause-button')
-
+const width = 9
 const squares = document.querySelectorAll('.grid div')
 
 let currentIndex = 76
 function moveFrog(e) {
-
+    squares[currentIndex].classList.remove('frog')
     switch(e.key) {
        case 'ArrowLeft' :
             console.log('move left')
+            currentIndex -= 1
             break
        case 'ArrowRight' :
             console.log('move right')
+            currentIndex += 1
             break
         case 'ArrowUp' :
             console.log('move up')
+            currentIndex -= width
             break
         case 'ArrowDown' :
             console.log('move Down')
+            currentIndex += width
              break
     }
     
