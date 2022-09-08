69c0a2cd02c6f1ed08f378c1dc78a37cf4b8ec5c
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-09-08 15:49:30 +0200

    Frog moves
    now we cant go outside the grid

--- app.js
+++ app.js
@@ -9,20 +9,20 @@ function moveFrog(e) {
     squares[currentIndex].classList.remove('frog')
     switch(e.key) {
        case 'ArrowLeft' :
-            console.log('move left')
-            currentIndex -= 1
+            
+            if(currentIndex % width !==0) currentIndex -= 1
             break
        case 'ArrowRight' :
-            console.log('move right')
-            currentIndex += 1
+            
+            if(currentIndex % width < width -1) currentIndex += 1
             break
         case 'ArrowUp' :
-            console.log('move up')
-            currentIndex -= width
+            
+            if (currentIndex - width >=0) currentIndex -= width
             break
         case 'ArrowDown' :
-            console.log('move Down')
-            currentIndex += width
+            
+            if (currentIndex + width < width * width) currentIndex += width
              break
     }
     
